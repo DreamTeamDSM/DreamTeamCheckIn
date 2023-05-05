e36752516f51f5e98cec7e635910e3ab01079437
Cortlan Bainbridge <cortlan.bainbridge@gmail.com>
2023-05-05 11:24:39 -0500

    updating styles I guess

--- src/RideInfo.js
+++ src/RideInfo.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Stack from '@mui/material/Stack';
 import Divider from '@mui/material/Divider';
 import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 import { MetadataBox } from './MetadataBox.js'
 
@@ -10,15 +11,28 @@ import { MetadataBox } from './MetadataBox.js'
 const RideInfo = () => {
     return (
         <>
-            <Box display='flex' sx={{ justifyContent: 'center', alignItems: 'center' }}>
-                <Stack direction="row" spacing={2} divider={<Divider orientation="vertical" flexItem />}>
+            <Typography component="h1" variant="h4" mt={5}
+                gutterBottom
+                color="inherit"
+                noWrap
+                sx={{ flexGrow: 1 }}> {'RIDES'}</Typography>
+            <Box border={1} borderRadius={'16px'} padding={2}>
+                <Box display="flex"
+                    flexWrap="wrap"
+                    justifyContent="space-around"
+                    alignItems="center"
+                >
                     <MetadataBox header={'Today\'s Date'} content={'05/05/2023'} />
+                    <Divider orientation="vertical" flexItem />
                     <MetadataBox header={'Location'} content={'Bike World'} />
+                    <Divider orientation="vertical" flexItem />
                     <MetadataBox header={'# Members'} content={'8'} />
+                    <Divider orientation="vertical" flexItem />
                     <MetadataBox header={'# Riders'} content={'12'} />
+                    <Divider orientation="vertical" flexItem />
                     <MetadataBox header={'Miles'} content={'32'} />
-                </Stack>
-            </Box>
+                </Box>
+            </Box >
         </>
     )
 }
