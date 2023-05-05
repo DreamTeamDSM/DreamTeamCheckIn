5b00ca4144dd84dee023e5676b989ac32b63a7be
Cortlan Bainbridge <cortlan.bainbridge@gmail.com>
2023-05-05 11:11:40 -0500

    basic front page-ish, adding ride info + metadaboxes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+import Container from '@mui/material/Container';
+import { RideInfo } from './RideInfo';
+
 
 function App() {
+  const mdTheme = createTheme({
+    palette: {
+      primary: {
+        light: '#757ce8',
+        main: '#3f50b5',
+        dark: '#002884',
+        contrastText: '#fff',
+      },
+      secondary: {
+        light: '#ff7961',
+        main: '#f44336',
+        dark: '#ba000d',
+        contrastText: '#000',
+      },
+    }
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. live from github actions or something
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={mdTheme}>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <header>
+          <Typography component="h1" variant="h4" mt={5}
+            gutterBottom
+            color="inherit"
+            noWrap
+            sx={{ flexGrow: 1 }}> {'Dream Team Check-in'}</Typography>
+        </header>
+        <main>
+          <RideInfo />
+        </main>
+      </Container>
+    </ThemeProvider>
   );
 }
 
