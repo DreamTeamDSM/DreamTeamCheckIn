ee54e11c07193507a0814d8c1c291060ea09c42e
Daniel Way <projects@danieldway.com>
2023-05-05 16:11:14 -0500

    Remove test auth debug option

--- src/components/DebugBar.js
+++ src/components/DebugBar.js
@@ -20,30 +20,6 @@ function DebugBar() {
     await saveDatabase(db);
   };
 
-  const testAuth = () => {
-    presentUserForToken((token) => {
-      try {
-        console.log("User authenticated. Updating token...", token);
-        gapi.client.setToken(token);
-
-        console.log("Fetching data...");
-        gapi.client.sheets.spreadsheets.values
-          .get({
-            spreadsheetId: "1MFzXHNw3-FOAKf0SUVQGXfWOWLCgXOSn_QW8sIu-ZvQ",
-            range: "A3:A12",
-          })
-          .then((response) => {
-            console.log("Response!");
-            const result = response.result;
-            const numRows = result.values ? result.values.length : 0;
-            console.log(`${numRows} rows retrieved: `, result.values);
-          });
-      } catch (err) {
-        console.log("Error fetching data: ", err);
-      }
-    });
-  };
-
   return (
     <div className="debug-bar">
       <button type="button" onClick={handleClick}>
@@ -56,7 +32,6 @@ function DebugBar() {
       </button>
       <button onClick={() => destroyDatabase()}>Destroy DB</button>
       <button onClick={() => getRides()}>Get Rides</button>
-      <button onClick={() => testAuth()}>Test Auth</button>
     </div>
   );
 }
