baeab28e35939f3f14752616dd3ea0f5ff733858
Daniel Way <projects@danieldway.com>
2023-05-05 16:44:12 -0500

    Stub export

---
+++ src/hooks/export.js
@@ -0,0 +1,25 @@
+const CLIENT_ID =
+  "592413971720-1psng6fqdu3dtn9hhvv1und82snfho3i.apps.googleusercontent.com";
+
+export async function export_data(db, onDbExported) {
+  const callback = (response) => {
+    const token = response.access_token;
+    gapi.client.setToken(token);
+
+    exportChecks();
+
+    onDbExported();
+  };
+
+  google.accounts.oauth2
+    .initTokenClient({
+      client_id: CLIENT_ID,
+      callback: callback,
+      scope: "https://www.googleapis.com/auth/spreadsheets",
+    })
+    .requestAccessToken();
+}
+
+function exportChecks(db) {
+  // TODO: write updated checks to sheets
+}
