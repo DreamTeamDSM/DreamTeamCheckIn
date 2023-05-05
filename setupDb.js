e19cdd507af5c1e1e92dfdfbf24d747dc842e105
ahallier <arhallier@gmail.com>
2023-05-04 23:08:40 -0500

    feat: add sqlite

---
+++ setupDb.js
@@ -0,0 +1,11 @@
+const sqlite3 = require('sqlite3');
+
+const db = new sqlite3.Database("db.sqlite", (err) => {
+    if (err) {
+      // Cannot open database
+      console.error(err.message);
+      throw err;
+    } else {
+      console.log("Connected to the SQLite database.");
+    }
+});
