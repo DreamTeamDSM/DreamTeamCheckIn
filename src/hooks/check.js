eb038284c7fb9c03da6d049cc53a6db710c308e0
ahallier <arhallier@gmail.com>
2023-05-05 23:29:03 -0500

    chore: groups assigments

--- src/hooks/check.js
+++ src/hooks/check.js
@@ -2,13 +2,13 @@ import { loadDatabase, saveDatabase } from '../database';
 
 export const check_in_participant = async (user_id, group_id) => {
     let db = await loadDatabase();
-    db.exec(`UPDATE GroupAssignment SET check_in=1 WHERE user_id=${user_id} AND group_id=${group_id}`);
+    db.exec(`UPDATE GroupAssignments SET check_in=1 WHERE user_id=${user_id} AND group_id=${group_id}`);
     await saveDatabase();
 }
 
 export const check_out_participant = async (user_id, group_id) => {
     let db = await loadDatabase();
-    db.exec(`UPDATE GroupAssignment SET check_out=1 WHERE user_id=${user_id} AND group_id=${group_id}`);
+    db.exec(`UPDATE GroupAssignments SET check_out=1 WHERE user_id=${user_id} AND group_id=${group_id}`);
     await saveDatabase();
 }
 
