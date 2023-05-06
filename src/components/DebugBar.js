85dc9bd2710603182f969fc54453ab5c3e7aa92b
ahallier <arhallier@gmail.com>
2023-05-06 13:37:01 -0500

    clean up debug bar

--- src/components/DebugBar.js
+++ src/components/DebugBar.js
@@ -5,20 +5,10 @@ import {
   loadDatabase,
   saveDatabaseAsFile,
 } from "../database.js";
-import { importData } from "../hooks/import";
-import { getRides, getRideById } from "../hooks/ride";
-import { useAppContext } from "../AppContext";
-import { export_data } from "../hooks/export";
 
 function DebugBar() {
   const queryParams = new URLSearchParams(location.search);
   const isDebugMode = queryParams.get('debug');
-  const { currentRide, refresh } = useAppContext();
-
-  const syncToDrive = () => {
-    export_data(currentRide.Ride.ride_id, () => console.log("Exported!"));
-    refresh();
-  }
 
   return isDebugMode && (
     <div className="debug-bar">
@@ -28,14 +18,6 @@ function DebugBar() {
         Download DB as File
       </button>
       <button onClick={() => destroyDatabase()}>Destroy DB</button>
-      <button onClick={() => getRides()}>Get Rides</button>
-      <button onClick={() => importData(() => console.log("Imported!"))}>
-        Sync FROM Drive
-      </button>
-      <button onClick={() => syncToDrive()}>
-        Sync TO Drive
-      </button>
-      <button onClick={() => getRideById(12)}>Get Ride 12</button>
     </div>
   );
 }
